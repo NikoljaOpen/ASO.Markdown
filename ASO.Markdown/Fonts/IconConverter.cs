80432ae91b1cac141e641dc86d3d52977e2eae67
LisenokArtist <lisenokartist@gmail.com>
2022-09-20 13:15:15 +0300

    настройка проекта для запуска теста библиотеки; добавлена библиотека с кастомными иконками;

---
+++ ASO.Markdown/Fonts/IconConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ASO.Markdown;
+
+public class IconConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is IconType iconType)
+        {
+            var icon = IconLibrary.Icons[iconType];
+
+            return icon;
+        }
+
+        return Binding.DoNothing;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
+
