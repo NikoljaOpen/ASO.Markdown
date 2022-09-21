315bcbff8b279c6259ac9da51f903694c1be4773
LisenokArtist <lisenokartist@gmail.com>
2022-09-21 15:03:06 +0300

    добавил компонент AsoMarkdownViewer; добавил в проект для тестов файл для тестов;

--- ASO.MarkdownTesting/MainWindow.xaml.cs
+++ ASO.MarkdownTesting/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using ASO.Markdown;
+using Markdig;
+using Markdig.Wpf;
 
 namespace ASO.MarkdownTesting
 {
@@ -23,6 +14,15 @@ namespace ASO.MarkdownTesting
         public MainWindow()
         {
             InitializeComponent();
+
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var path = "../../../Documents/Markdig-readme.md";
+
+            Viewer.Path = path;
         }
     }
 }
